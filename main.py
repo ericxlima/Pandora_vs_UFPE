97e4fdde313eead051bcf4ab44db89263eb71b86
eric_lima <eric.vinlima@gmail.com>
2021-08-12 22:28:44 -0300

    feat: create the game base with movimentation, and some constants

---
+++ main.py
@@ -0,0 +1,42 @@
+from classes.pandora import Pandora
+import pygame
+
+from scripts.constants import LENGHT_SCREEN_MEDIUM
+
+
+#  Inicialize the Package, and a Screen Title
+pygame.init()
+pygame.display.set_caption('Pandora vs UFPE')
+
+#  Set the Screen Size, and load the background 
+screen = pygame.display.set_mode(size=LENGHT_SCREEN_MEDIUM)
+background = pygame.image.load('media/bg_geral.png')
+
+#  For not to import this module
+if __name__ == '__main__':
+    
+    #  Inicialize Pandora
+    pandora = Pandora()
+
+    #  Game-Loop
+    running = True
+    while running:
+
+        #  Calculate Rules:
+        coordinates = pandora.x, pandora.y
+
+        #  Print in the Screen
+        screen.blit(background, (0, 0))
+        screen.blit(pandora.draw, coordinates)
+        pygame.display.update()
+        
+        #  Check the Events
+
+        events = pygame.event.get()
+        
+        for event in events:
+            if event.type == pygame.QUIT:
+                running = False
+        
+        pandora.event_processor(events)
+
---
+++ scripts/constants.py
@@ -0,0 +1,15 @@
+"""
+Document for geral contants 
+"""
+
+#  Lengh for screens in scale 16x9
+LENGHT_SCREEN_LOW = (640, 360)
+LENGHT_SCREEN_MEDIUM = (854, 480)
+LENGHT_SCREEN_HD = (1280, 720)
+
+#  Some Colors
+YELLOW = (255, 255, 0)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
